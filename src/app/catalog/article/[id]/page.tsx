d4560cf7826864b9a035cb0a00d62b75130be43d
ratiborU <tvinck_444@mail.ru>
2024-06-16 20:42:58 +0500

    Update meta for articles

--- src/app/catalog/article/[id]/page.tsx
+++ src/app/catalog/article/[id]/page.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { getArticleById } from '@/data/AticleApi';
-
+import { transformDate } from '@/lib/utils';
 import ArticlePageClient from '@/components/pages/ArticlePage';
 import { Metadata } from 'next';
 import { frontUrl } from '@/lib/utils';
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-
   const article = await getArticleById(params.id);
-  const title = article?.title.Ru;
-  const description = article?.content.Ru;
+  const keywords = article?.keywords.map(x => x.Ru).join(', ');
 
+  const title = article?.title.Ru;
+  const description = `${article?.content.Ru}. Авторы: ${article?.authors.map(x => x.fullname.Ru).join(', ')}. Дата поступления: ${transformDate(article?.dateReceipt)}, Дата принятия: ${transformDate(article?.dateAcceptance)}`;
   return {
     title: title,
     description: description,
+    keywords, 
     openGraph: {
       type: "website",
       url: `${frontUrl}/catalog/article/${params.id}`,
