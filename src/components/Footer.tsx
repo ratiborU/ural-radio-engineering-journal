fd54e8ce0eb39c9314749a661c48be893598c7ed
ratiborU <tvinck_444@mail.ru>
2024-06-18 23:16:17 +0500

    footer year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Footer = () => {
+  const data = new Date();
   return (
     <footer className='footer'>
-      <div className="footer__text">2024 г. Издательство Уральского университета Россия, 620083, Екатеринбург, ул. Тургенева, 4</div>
+      <div className="footer__text">{data.getFullYear()} г. Издательство Уральского университета Россия, 620083, Екатеринбург, ул. Тургенева, 4</div>
     </footer>
   );
 };
